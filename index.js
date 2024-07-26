058bce4e84cd60f4dae0c771c7ac410c7c0a94dd
ajmal shahan <ajmalshahan23@gmail.com>
2024-07-26 16:17:54 +0530

    added new route ajmal

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const PORT =process.env.PORT || 3000
 app.get("/", (req, res) => {
     res.send("<h1>hi</h1>")
 })
+app.get("/ajmal",(req,res)=>{
+    res.send("Hi from ajmal")
+})
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT} `);
